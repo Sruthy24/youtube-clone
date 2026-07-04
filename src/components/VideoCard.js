d3f4632acb53dc96f55cd3ca1055d3cb1f174b54
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-04 20:53:16 +0530

    Refactor VideoCard component for better readability

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,38 +1,62 @@
 import React from "react";
-import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography
+} from "@mui/material";
 
-function VideoCard({video}){
+import { useNavigate } from "react-router-dom";
 
-return(
+function VideoCard({ video }) {
 
-<Card sx={{width:320}}>
+  const navigate = useNavigate();
 
-<CardMedia
-component="img"
-height="180"
-image={video.image}
-/>
+  if (!video.id?.videoId) return null;
 
-<CardContent>
+  return (
+    <Card
+      onClick={() => navigate(`/video/${video.id.videoId}`)}
+      sx={{
+        background: "#0f0f0f",
+        color: "white",
+        boxShadow: "none",
+        cursor: "pointer"
+      }}
+    >
 
-<Typography variant="h6">
-{video.title}
-</Typography>
+      <CardMedia
+        component="img"
+        image={video.snippet.thumbnails.high.url}
+        height="200"
+        sx={{
+          borderRadius: "15px"
+        }}
+      />
 
-<Typography color="gray">
-{video.channel}
-</Typography>
+      <CardContent>
 
-<Typography variant="body2">
-{video.views}
-</Typography>
+        <Typography
+          fontWeight="bold"
+          sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap"
+          }}
+        >
+          {video.snippet.title}
+        </Typography>
 
-</CardContent>
+        <Typography color="gray">
 
-</Card>
+          {video.snippet.channelTitle}
 
-);
+        </Typography>
 
+      </CardContent>
+
+    </Card>
+  );
 }
 
 export default VideoCard;
