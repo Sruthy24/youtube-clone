b3db4eb2418a4229cbf6a2a8656dd2e0b83b48a3
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-04 20:58:59 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,20 @@
+import axios from "axios";
+import { API_KEY, BASE_URL } from "./constants";
 
+export const fetchVideos = async (query) => {
+
+  const { data } = await axios.get(
+    `${BASE_URL}/search`,
+    {
+      params: {
+        part: "snippet",
+        maxResults: 20,
+        q: query,
+        type: "video",
+        key: API_KEY
+      }
+    }
+  );
+
+  return data;
+};
