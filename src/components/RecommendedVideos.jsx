246fccf3cafefc75a0ff0ad89c9e7eea7fe2ccf5
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-04 21:06:37 +0530

    Update RecommendedVideos.jsx

--- src/components/RecommendedVideos.jsx
+++ src/components/RecommendedVideos.jsx
@@ -1,1 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import VideoCard from "./VideoCard";
+import { fetchVideos } from "../utils/api";
 
+function RecommendedVideos() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+
+    fetchVideos("popular")
+
+      .then((data) => setVideos(data.items))
+
+      .catch(console.log);
+
+  }, []);
+
+  return (
+
+    <Box
+      sx={{
+        width: "380px",
+        ml: 2,
+        display: "flex",
+        flexDirection: "column",
+        gap: 2
+      }}
+    >
+
+      {videos.map((video) => (
+
+        <VideoCard
+
+          key={video.id.videoId}
+
+          video={video}
+
+        />
+
+      ))}
+
+    </Box>
+
+  );
+}
+
+export default RecommendedVideos;
