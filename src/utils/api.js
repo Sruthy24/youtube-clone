9f0e8bb602ef28f063fa576c08f2637f60478c6e
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-05 11:36:54 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,49 @@
 import axios from "axios";
 import { API_KEY, BASE_URL } from "./constants";
 
-export const fetchVideos = async (query) => {
+const api = axios.create({
+  baseURL: BASE_URL,
+});
 
-  const { data } = await axios.get(
-    `${BASE_URL}/search`,
-    {
-      params: {
-        part: "snippet",
-        maxResults: 20,
-        q: query,
-        type: "video",
-        key: API_KEY
-      }
-    }
-  );
+// Search videos
+export const searchVideos = async (query) => {
+  const { data } = await api.get("/search", {
+    params: {
+      part: "snippet",
+      maxResults: 20,
+      q: query,
+      type: "video",
+      key: API_KEY,
+    },
+  });
 
-  return data;
+  return data.items;
+};
+
+// Get video details
+export const getVideoDetails = async (id) => {
+  const { data } = await api.get("/videos", {
+    params: {
+      part: "snippet,statistics",
+      id,
+      key: API_KEY,
+    },
+  });
+
+  return data.items[0];
+};
+
+// Get related videos
+export const getRelatedVideos = async (id) => {
+  const { data } = await api.get("/search", {
+    params: {
+      part: "snippet",
+      relatedToVideoId: id,
+      type: "video",
+      maxResults: 15,
+      key: API_KEY,
+    },
+  });
+
+  return data.items;
 };
