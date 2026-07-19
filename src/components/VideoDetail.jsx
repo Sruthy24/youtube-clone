b0738a3842c79a5b6ea0d65cfd49160980f07235
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-19 12:55:00 +0530

    Update VideoDetail.jsx

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
-import { useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
-import { getVideoDetails } from "../utils/api";
-import RecommendedVideos from "./RecommendedVideos";
+import { useParams } from "react-router-dom";
+import { getVideoDetails, searchVideos } from "../utils/api";
+import VideoCard from "./VideoCard";
 
 function VideoDetail() {
 
   const { id } = useParams();
 
   const [video, setVideo] = useState(null);
+  const [relatedVideos, setRelatedVideos] = useState([]);
 
   useEffect(() => {
-    getVideoDetails(id).then((data)=>{
-setVideo(data);
-});
+
+    getVideoDetails(id)
+      .then((data) => setVideo(data));
+
+    searchVideos("popular")
+      .then((data) => setRelatedVideos(data));
+
   }, [id]);
+
+  if (!video) return <Typography color="white">Loading...</Typography>;
 
   return (
 
@@ -26,60 +33,63 @@ setVideo(data);
           xs: "column",
           lg: "row"
         },
-        p: 2,
-        background: "#0f0f0f",
-        minHeight: "100vh"
+        gap: 3,
+        p: 3,
+        background: "#0f0f0f"
       }}
     >
 
-      <Box sx={{ flex: 2 }}>
+      <Box flex={1}>
 
         <ReactPlayer
-
           url={`https://www.youtube.com/watch?v=${id}`}
-
           controls
-
           width="100%"
-
-          height="600px"
-
+          height="60vh"
         />
 
-        {video && (
+        <Typography
+          sx={{
+            mt: 2,
+            color: "white",
+            fontWeight: "bold",
+            fontSize: {
+              xs: "20px",
+              md: "28px"
+            }
+          }}
+        >
+          {video.snippet.title}
+        </Typography>
 
-          <>
-
-            <Typography
-              variant="h5"
-              sx={{
-                mt: 2,
-                color: "white",
-                fontWeight: "bold"
-              }}
-            >
-              {video.snippet.title}
-            </Typography>
-
-            <Typography
-              sx={{
-                color: "gray",
-                mt: 1
-              }}
-            >
-              {video.snippet.channelTitle}
-            </Typography>
-
-          </>
-
-        )}
+        <Typography
+          sx={{
+            color: "#aaa",
+            mt: 1
+          }}
+        >
+          {video.snippet.channelTitle}
+        </Typography>
 
       </Box>
 
-      <RecommendedVideos />
+      <Box
+        sx={{
+          width: {
+            xs: "100%",
+            lg: "380px"
+          }
+        }}
+      >
+        {relatedVideos.map((item) => (
+          <VideoCard
+            key={item.id.videoId}
+            video={item}
+          />
+        ))}
+      </Box>
 
     </Box>
-
   );
 }
 
